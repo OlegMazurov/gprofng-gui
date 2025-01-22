0156d3b5522b1e1d4e6197240fcf3ab3146a86d4
Vladimir Mezentsev <vladimir.mezentsev@oracle.com>
2025-01-21 20:03:49 -0800

    Fix Copyright

--- gprofng-gui.sh
+++ gprofng-gui.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2022-2024 Free Software Foundation
+# Copyright (C) 2022-2025 Free Software Foundation
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
